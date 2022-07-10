09a2361e345ffb34ee61fc9b7fa0c976b3b76766
fmmhassan <fmmhassan@gmail.com>
2022-07-10 22:44:01 +0400

    done

--- claromentis-frontend/src/App.js
+++ claromentis-frontend/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Dashboard from './components/dashboard/dashboard';
-require('dotenv').config()
+
 
  
 function App(){
-  console.log('das') // remove this after you've confirmed it working
-  console.log(process.env) // remove this after you've confirmed it working
+  
   return(
     <div>
       <Dashboard></Dashboard>
